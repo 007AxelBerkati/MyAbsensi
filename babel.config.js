f0750feeb9e66b0fe1edd6c26bd343f8bbd4268c
007AxelBerkati <bucel1916@gmail.com>
2022-10-15 13:43:11 +0700

    finally babel can useeee

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  // plugins: [
+  //   [
+  //     'module-resolver',
+  //     {
+  //       root: ['./src'],
+  //       extensions: [
+  //         '.ts',
+  //         '.tsx',
+  //         '.jsx',
+  //         '.js',
+  //         '.jsx',
+  //         '.json',
+  //         '.svg',
+  //         '.jpg',
+  //       ],
+  //       alias: {
+  //         components: './src/components',
+  //       },
+  //     },
+  //   ],
+  // ],
 };
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Hallo</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
