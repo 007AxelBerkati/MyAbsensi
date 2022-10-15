ea350c609b91792d65d0449971b499423048b19b
007AxelBerkati <bucel1916@gmail.com>
2022-10-15 19:47:59 +0700

    setup yup

---
+++ src/plugins/yup/index.tsx
@@ -0,0 +1,9 @@
+import * as Yup from 'yup';
+
+export const loginSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Email Tidak Valid')
+    .required('Tolong isi Email Anda')
+    .trim(),
+  password: Yup.string().required('Tolong Masukan Password Anda').trim(),
+});
