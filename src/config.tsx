412c756b4f7cef473dc06afba0dcc190bda37149
007AxelBerkati <bucel1916@gmail.com>
2022-10-16 10:35:04 +0700

    setup env

---
+++ src/config.tsx
@@ -0,0 +1,7 @@
+// export default {
+//   backendApi:
+//     // eslint-disable-next-line no-undef
+//     process.env.NODE_ENV === 'production'
+//       ? 'https://market-final-project.herokuapp.com'
+//       : 'https://market-final-project.herokuapp.com',
+// };
