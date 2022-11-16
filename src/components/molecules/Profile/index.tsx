5c59ab915be7a6599e1ecf85e6738bf33a06149f
007AxelBerkati <bucel1916@gmail.com>
2022-11-16 23:26:47 +0700

    add profile

---
+++ src/components/molecules/Profile/index.tsx
@@ -0,0 +1,61 @@
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import FastImage from 'react-native-fast-image';
+import React from 'react';
+import {IconRemovePhoto, ILNullPhoto} from '../../../assets';
+import {windowHeight, windowWidth} from '../../../theme';
+
+type ProfileProps = {
+  isRemove?: boolean;
+  onPress?: () => void;
+  source?: any;
+};
+
+function Profile({isRemove, source, onPress}: ProfileProps) {
+  return (
+    <View style={styles.photoSection}>
+      {!isRemove && (
+        <View style={styles.photo}>
+          <FastImage
+            source={source !== null ? source : ILNullPhoto}
+            style={styles.avatar}
+          />
+          {isRemove && <IconRemovePhoto style={styles.removePhoto} />}
+        </View>
+      )}
+      {isRemove && (
+        <TouchableOpacity style={styles.photo} onPress={onPress}>
+          <FastImage source={source || ILNullPhoto} style={styles.avatar} />
+          {isRemove && <IconRemovePhoto style={styles.removePhoto} />}
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+}
+
+export default Profile;
+
+const styles = StyleSheet.create({
+  photo: {
+    width: windowWidth * 0.3,
+    height: windowHeight * 0.15,
+    borderRadius: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+  },
+
+  photoSection: {
+    alignItems: 'center',
+    marginVertical: 24,
+  },
+  avatar: {
+    width: windowWidth * 0.3,
+    height: windowHeight * 0.15,
+    alignSelf: 'center',
+  },
+  removePhoto: {
+    position: 'absolute',
+    right: 8,
+    bottom: 8,
+  },
+});
