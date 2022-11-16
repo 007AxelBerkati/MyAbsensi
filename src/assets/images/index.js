185849bab622299c18a91c9e116bc3fae1661743
007AxelBerkati <bucel1916@gmail.com>
2022-11-16 23:27:32 +0700

    add library and image

--- src/assets/images/index.js
+++
@@ -1,5 +0,0 @@
-import IconsApp2 from './IconsApp2.png';
-import IconsApp from './IconsApp.svg';
-import Splash from './Splash.json';
-
-export {IconsApp2, IconsApp, Splash};
---
+++ src/assets/images/index.ts
@@ -0,0 +1,7 @@
+import IconsApp2 from './IconsApp2.png';
+import IconsApp from './IconsApp.svg';
+import Splash from './Splash.json';
+import IconRemovePhoto from './ic-remove-photo.svg';
+import ILNullPhoto from './null-photo.png';
+
+export {IconsApp2, IconsApp, Splash, IconRemovePhoto, ILNullPhoto};
