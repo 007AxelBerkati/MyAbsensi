c9bd20bb697f3476762aa66000bae7077c18bef1
007AxelBerkati <bucel1916@gmail.com>
2022-11-16 23:27:16 +0700

    add component molecules

---
+++ src/components/molecules/index.ts
@@ -0,0 +1,6 @@
+import CardList from './CardList';
+import Headers from './Headers';
+import Loading from './Loading';
+import Profile from './Profile';
+
+export {CardList, Headers, Loading, Profile};
