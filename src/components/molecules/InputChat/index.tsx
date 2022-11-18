0918127d79f594b9cfcea0d74ca4b244f7afd830
007AxelBerkati <bucel1916@gmail.com>
2022-11-18 23:10:04 +0700

    add component input chat

---
+++ src/components/molecules/InputChat/index.tsx
@@ -0,0 +1,56 @@
+import React, {useEffect} from 'react';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {COLORS, FONTS} from '../../../theme';
+import {CustomButton} from '../../atoms';
+
+type InputChatProps = {
+  value: string;
+  onChangeText: (value: string) => void;
+  onButtonPress: () => void;
+  targetChat: any;
+};
+
+export default function InputChat({
+  value,
+  onChangeText,
+  onButtonPress,
+  targetChat,
+}: InputChatProps) {
+  // useEffect(() => {
+  //   console.log('targetChat', targetChat);
+  // }, []);
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder={`Tulis Pesan Untuk ${targetChat?.fullname}`}
+        value={value}
+        onChangeText={onChangeText}
+      />
+      <CustomButton
+        type={'Icon-send'}
+        onPress={onButtonPress}
+        disable={value.length < 1}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    flexDirection: 'row',
+    backgroundColor: COLORS.background.primary,
+  },
+  input: {
+    backgroundColor: COLORS.disable.background,
+    padding: 14,
+    borderRadius: 10,
+    flex: 1,
+    marginRight: 10,
+    fontFamily: FONTS.primary.normal,
+    fontSize: 14,
+    maxHeight: 45,
+  },
+});
