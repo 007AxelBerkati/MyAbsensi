77767d9f49a7a102220a3148cb713e495c5a03e1
007AxelBerkati <bucel1916@gmail.com>
2022-11-18 23:10:23 +0700

    update navigation

--- src/navigations/Stack.tsx
+++ src/navigations/Stack.tsx
@@ -1,5 +1,14 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Dashboard, ForgetPass, Login, SplashScreen} from '../pages';
+import {
+  AllUser,
+  Chatting,
+  Dashboard,
+  EditProfile,
+  ForgetPass,
+  InformasiProfile,
+  Login,
+  SplashScreen,
+} from '../pages';
 import React from 'react';
 import MainApp from './MainApp';
 
@@ -16,6 +25,10 @@ function Router() {
       <Stack.Screen name="Dashboard" component={MainApp} />
       <Stack.Screen name="SplashScreen" component={SplashScreen} />
       <Stack.Screen name="ForgetPass" component={ForgetPass} />
+      <Stack.Screen name="InformasiProfile" component={InformasiProfile} />
+      <Stack.Screen name="EditProfile" component={EditProfile} />
+      <Stack.Screen name="AllUser" component={AllUser} />
+      <Stack.Screen name="Chatting" component={Chatting} />
       {/* <Stack.Screen name="Geo" component={Geo} /> */}
     </Stack.Navigator>
   );
