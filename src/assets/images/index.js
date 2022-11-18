d43ac0e2bc9d05b5569c6d52430a5e99ffd96311
007AxelBerkati <bucel1916@gmail.com>
2022-11-18 23:08:58 +0700

    add icon and images

---
+++ src/assets/images/index.js
@@ -0,0 +1,35 @@
+import IconsApp2 from './IconsApp2.png';
+import IconsApp from './IconsApp.svg';
+import Splash from './Splash.json';
+import IconRemovePhoto from './ic-remove-photo.svg';
+import ILNullPhoto from './null-photo.png';
+import IconRate from './ic-rate.svg';
+import IconHelp from './ic-help.svg';
+import IconEditProfile from './ic-edit-profile.svg';
+import IconNext from './ic-next.svg';
+import IconLanguage from './ic-language.svg';
+import BackgroundChat from './BackgroundChat.png';
+import IconBackDark from './ic-back-dark.svg';
+import IconBackLight from './ic-back-light.svg';
+import IconSendDark from './ic-send-dark.svg';
+import IconSendLight from './ic-send-light.svg';
+import IconAddPhoto from './ic-add-photo.svg';
+
+export {
+  IconsApp2,
+  IconsApp,
+  Splash,
+  IconRemovePhoto,
+  ILNullPhoto,
+  IconRate,
+  IconHelp,
+  IconEditProfile,
+  IconNext,
+  IconLanguage,
+  BackgroundChat,
+  IconBackDark,
+  IconBackLight,
+  IconSendDark,
+  IconSendLight,
+  IconAddPhoto,
+};
